166fa069bc45428cc0b733fff4130308db4595fd
paramwork <63786245+paramwork@users.noreply.github.com>
2023-01-12 18:42:39 +0530

    git config --global --add safe.directory /github/workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ RepositoryName="${INPUT_REPOSITORY_NAME}"
 AwsRegion="${INPUT_AWS_REGION}"
 CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
 
+git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git remote add sync ${CodeCommitUrl}
