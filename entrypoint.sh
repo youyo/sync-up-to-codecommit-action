683556d799463ea5b0ca37d8968c8f368fdc901b
Sin <13870295+sino1641@users.noreply.github.com>
2024-02-21 02:03:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,4 +16,4 @@ git config --global --add safe.directory /github/workspace
 git config --global credential."https://git-codecommit.${AwsRegion}.${AwsDomain}".helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git remote add sync ${CodeCommitUrl}
-git push sync --mirror
+git push sync --all
